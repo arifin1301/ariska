85a960bd43637975d17691a6708c4ba0c650e975
kamal <essepop629@gmail.com>
2024-09-18 02:19:05 +0800

    add page program

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'for kak ariska cakep'.split('')
+const text = 'for kak ariska'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
